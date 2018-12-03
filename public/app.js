d985921bf281665b775805e40c2b18e242b563f2
Maksim Podvarkov <i.snova.bebebe@gmail.com>
2018-12-03 11:17:08 +0500

    Sign user out

--- public/app.js
+++ public/app.js
@@ -12,7 +12,7 @@ var request = function (params, success, err) {
 
   xhr.open(params.method, params.url)
   if (params.headers) {
-    for (var i = 0; i<params.headers.length; i++) {
+    for (var i = 0; i < params.headers.length; i++) {
       console.log(params.headers[i])
       xhr.setRequestHeader(params.headers[i][0], params.headers[i][1])
     }
@@ -23,7 +23,7 @@ var request = function (params, success, err) {
 }
 
 var app = {}
-app.createAccount = function(form) {
+app.createAccount = function (form) {
   var formError = document.querySelector('.formError')
   formError.style.display = 'none'
 
@@ -39,7 +39,7 @@ app.createAccount = function(form) {
     }
   }
 
-  request(params, function(){
+  request(params, function () {
     window.location.href = "/"
   }, function (err) {
     console.error(err)
@@ -48,10 +48,71 @@ app.createAccount = function(form) {
   })
 }
 
-document.addEventListener('submit', function (e) {
-  e.preventDefault()
-  switch (e.target.id) {
-    case 'accountCreate':
-      return app.createAccount(e.target)
+app.createSession = function (form) {
+  var formError = document.querySelector('.formError')
+  formError.style.display = 'none'
+
+  var params = {
+    url: form.action,
+    method: form.method,
+    body: {
+      username: form.elements['userName'].value,
+      password: form.elements['password'].value,
+    }
   }
-})
+
+  request(params, function (data) {
+    localStorage.setItem('token', data.token)
+    window.location.href = '/'
+  }, function (err) {
+    console.error(err)
+    formError.innerText = err.message
+    formError.style.display = 'block'
+  })
+}
+
+var checkSession = function () {
+  if (localStorage.getItem('token')) {
+    document.body.classList.add('loggedIn')
+    return localStorage.getItem('token')
+  } else {
+    document.body.classList.remove('loggedIn')
+    return undefined
+  }
+}
+
+setFormListeners = function () {
+  document.addEventListener('submit', function (e) {
+    e.preventDefault()
+    switch (e.target.id) {
+      case 'accountCreate':
+        return app.createAccount(e.target)
+      case 'sessionCreate':
+        return app.createSession(e.target)
+    }
+  })
+}
+
+var signUserOut = function () {
+  request({
+    headers: [['Authorization', 'Bearer ' + app.sessionToken]],
+    method: 'DELETE',
+    url: '/api/session'
+  }, function () {
+    localStorage.removeItem('token')
+    window.location.href = '/'
+  }, function (err) {
+    console.log(err.message)
+  })
+}
+
+var bindLogOut = function () {
+  var btn = document.getElementById('logoutButton');
+  btn.addEventListener('click', signUserOut)
+}
+
+window.onload = function () {
+  app.sessionToken = checkSession()
+  setFormListeners()
+  bindLogOut()
+}
